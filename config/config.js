db0693e557dd6565a4e53941f2d239443346fb9e
bin wei <binwei1988@gmail.com>
2018-10-15 12:06:19 -0400

    clear

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 let env = process.env.NODE_ENV || 'development';
 
-if (env === 'development' || env === 'test') {
+if (env === 'development' || env === 'production') {
   let config = require('./config.json');
   let envConfig = config[env];
   Object.keys(envConfig).forEach((key) => {
